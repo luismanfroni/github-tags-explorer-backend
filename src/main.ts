88559f4f2e35c6305443956fc4a66b72c70cd210
Luis Felipe Manfroni <me@luismanfroni.com>
2021-05-10 16:18:42 -0300

    feat: add filter for httpexceptions

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,25 @@ import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import genOrmConfig from "utils/genOrmConfig";
+import { HttpExceptionFilter } from "./utils/HttpExceptionFilter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix("api");
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
   const config = app.get(ConfigurationService);
   if (!config.isProduction()) {
-  const document = SwaggerModule.createDocument(
-    app,
-    new DocumentBuilder()
-      .setTitle("Tags API")
-      .setDescription("Github Tags Explorer API Documentation")
-      .build()
-  );
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle("Tags API")
+        .setDescription("Github Tags Explorer API Documentation")
+        .build()
+    );
 
-  SwaggerModule.setup("docs", app, document);
+    SwaggerModule.setup("docs", app, document);
   }
   genOrmConfig(config.getTypeOrmConfig());
   await app.listen(config.getPort());
