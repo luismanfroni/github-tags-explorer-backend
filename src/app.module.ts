6087742664cf29b31b359aa6277ff7134411df69
Luis Felipe Manfroni <me@luismanfroni.com>
2021-05-12 16:55:58 -0300

    feat: added github module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigurationModule } from "./config/config.module";
 import { AuthModule } from "modules/authentication/auth.module";
+import { GithubModule } from "modules/github/github.module";
 
 @Module({
-  imports: [ConfigurationModule, AuthModule]
+  imports: [ConfigurationModule, GithubModule, AuthModule]
 })
 export class AppModule {}
